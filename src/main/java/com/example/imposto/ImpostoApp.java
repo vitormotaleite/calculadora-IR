733e6dba64a1d32c2f1b3690deb9e6fb67f47e13
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-06-03 11:39:35 -0300

    controller adicionado

---
+++ src/main/java/com/example/imposto/ImpostoApp.java
@@ -0,0 +1,14 @@
+package com.example.imposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpostoApp.class, args);
+    }
+    
+}
+    
--- src/main/java/com/example/imposto/controller/ImpostoController.java
+++ src/main/java/com/example/imposto/controller/ImpostoController.java
@@ -1,10 +1,25 @@
 package com.example.imposto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.imposto.calculoIR.CalculoIrResponse;
+import com.example.imposto.calculoIR.CalcuuloIrRequest;
+import com.example.imposto.service.CalculadoraService;
 
 @RestController
 @RequestMapping("/api/imposto")
 public class ImpostoController {
+
+    @Autowired
+    private CalculadoraService calculadoraService;
+
+    @PostMapping
+    public CalculoIrResponse calcular(@RequestBody CalcuuloIrRequest req) {
+        return calculadoraService.calcularImposto(req);
+    }
     
 }
