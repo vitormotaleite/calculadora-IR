443ed7d66202813a66f3e901a2aa33b8ca1ffa22
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-06-03 10:45:24 -0300

    FaixaIR adicionado

---
+++ src/main/java/com/example/imposto/model/FaixaIR.java
@@ -0,0 +1,41 @@
+package com.example.imposto.model;
+
+public class FaixaIR {
+
+    private Double limiteSuperior;
+    private double aliquota;
+    private double deducao;
+    
+    public FaixaIR() {
+    }
+
+    public FaixaIR(Double limiteSuperior, double aliquota, double deducao) {
+        this.limiteSuperior = limiteSuperior;
+        this.aliquota = aliquota;
+        this.deducao = deducao;
+    }
+
+    public Double getLimiteSuperior() {
+        return limiteSuperior;
+    }
+
+    public void setLimiteSuperior(Double limiteSuperior) {
+        this.limiteSuperior = limiteSuperior;
+    }
+
+    public double getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(double aliquota) {
+        this.aliquota = aliquota;
+    }
+
+    public double getDeducao() {
+        return deducao;
+    }
+
+    public void setDeducao(double deducao) {
+        this.deducao = deducao;
+    }
+}
