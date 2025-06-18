5cb654bb5eac983fc0329421aae11c02f9b02299
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-06-18 13:42:56 -0300

    frontend iniciado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./Styles.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [form, setForm] = useState({
+    ano: 2025,
+    rendaAnual: "",
+    dependentes: "",
+    despesasInstrucao: "",
+    simplificado: false,
+  });
 
-export default App;
+  const [resultado, setResultado] = useState(null);
+
+  const handleChange = (e) => {
+    const {name, value, type, checked} = e.target;
+    setForm({
+      ...form,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("")
+  }
+}
