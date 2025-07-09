c24b8700a5fbf06148357599f56f487181bfb424
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2025-07-09 18:32:56 -0300

    autenticação finalizada

--- backend/src/main/java/com/example/imposto/auth/JwtUtil.java
+++ backend/src/main/java/com/example/imposto/auth/JwtUtil.java
@@ -1,22 +1,24 @@
 package com.example.imposto.auth;
 
+import java.security.Key;
 import java.util.Date;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUtil {
 
-    private static final String SECRET = "segredo-seguro";
+    private static final Key SECRET = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     private static final long EXPIRATION_TIME = 86400000;
 
     public static String gerarToken(String username) {
         return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-        .signWith(SignatureAlgorithm.HS512, SECRET).compact();
+                .signWith(SECRET).compact();
     }
 
     public static String validarToken(String token) {
         return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody().getSubject();
     }
-    
+
 }
--- backend/src/main/java/com/example/imposto/controller/AuthController.java
+++ backend/src/main/java/com/example/imposto/controller/AuthController.java
@@ -3,12 +3,11 @@ package com.example.imposto.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.imposto.auth.JwtUtil;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/auth")
